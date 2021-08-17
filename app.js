666cd3052682138719c2b985e14e47509ea1f1f1
Callum Byrne <callumjbyrne97@gmail.com>
2021-08-17 23:34:33 +1000

    setup files

---
+++ app.js
@@ -0,0 +1,16 @@
+
+//gameBoard module
+const gameBoard = (() => {
+    let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+})();
+
+//displayController module
+
+
+//Players factory function
+const Players
+
+
+const player1
+
+const player2
